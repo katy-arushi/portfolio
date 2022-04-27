7e8db3dffcb8ea8bbeebbfdeeaf0ddb51b1652a7
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 15:50:36 -0400

    use input-bg theme color for text color, rather than custom color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 			{/* container */}
 			<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
 				<p className='text-pink-600'>Hi, my name is</p>
-				<h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+				<h1 className='text-4xl sm:text-7xl font-bold text-input-bg'>
 					Arushi Katyal
 				</h1>
 				<h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
