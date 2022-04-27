144cd29036a4191b2d5e3628ab2f6fa9e3853b14
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 16:48:16 -0400

    bold text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -88,7 +88,7 @@ function NavBar() {
 			</ul>
 
 			{/* social icons */}
-			<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+			<div className='hidden lg:flex fixed flex-col top-[35%] left-0 font-bold'>
 				<ul>
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
 						<a
