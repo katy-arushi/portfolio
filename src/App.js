83379a25c4a31a64efd39b17d7aacb126d60cbb0
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 01:07:51 -0400

    add skills component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-      <About/>
+      <About />
+      <Skills />
     </div>
   );
 }
