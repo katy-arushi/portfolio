039f0c63ee6e7cfbd477c716cc07468d43c4418c
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 16:51:30 -0400

    make text larger

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,7 +14,9 @@ const Work = () => {
 					<p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
 						Work
 					</p>
-					<p className='py-6'>// Check out some of my recent projects</p>
+					<p className='py-6 text-lg'>
+						// Check out some of my recent projects
+					</p>
 				</div>
 
 				{/* container */}
