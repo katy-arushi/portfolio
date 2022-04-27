c13fc1cd237061ad5f830072f6761cd4af2ba7fd
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 15:45:54 -0400

    use background color from theme rather than custom color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll';
 
 const Home = () => {
 	return (
-		<div name='home' className='w-full h-screen bg-[#0a192f]'>
+		<div name='home' className='w-full h-screen bg-background'>
 			{/* container */}
 			<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
 				<p className='text-pink-600'>Hi, my name is</p>
@@ -22,12 +22,12 @@ const Home = () => {
 				</p>
 				<div>
 					<Link to='work' smooth={true} duration={500}>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-              View Work
-              <span className='group-hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-3' />
-              </span>
-            </button>
+						<button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+							View Work
+							<span className='group-hover:rotate-90 duration-300'>
+								<HiArrowNarrowRight className='ml-3' />
+							</span>
+						</button>
 					</Link>
 				</div>
 			</div>
