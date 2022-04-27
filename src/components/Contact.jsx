4f1b02e07453ef3235dd71fe4d3de178e027bd8c
Arushi Katyal <katy.arushi@gmail.com>
2022-04-27 02:11:30 -0400

    add getform endpoint to submit form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
 			name='contact'
 			className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'
 		>
-			<form action='' className='flex flex-col max-w-[600px] w-full'>
+			<form
+				method='POST'
+				action='https://getform.io/f/0a1bbaf0-1c19-43d0-b584-b52e406c4e5d'
+				className='flex flex-col max-w-[600px] w-full'
+			>
 				<div className='pb-8'>
 					<p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
 						Contact
@@ -33,7 +37,9 @@ const Contact = () => {
 					rows='10'
 					placeholder='Message'
 				></textarea>
-				<button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+				<button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>
+					Let's Collaborate
+				</button>
 			</form>
 		</div>
 	);
