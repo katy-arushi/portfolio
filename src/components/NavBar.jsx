e53a8473240d782b086514ef2bfc8c6966a48907
Arushi Katyal <katy.arushi@gmail.com>
2022-04-29 02:39:50 -0400

    edit height on page of social icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -148,7 +148,7 @@ function NavBar() {
 			</ul>
 
 			{/* social icons */}
-			<div className='hidden lg:flex fixed flex-col top-[35%] left-0 font-bold'>
+			<div className='hidden lg:flex fixed flex-col top-[38%] left-0 font-bold'>
 				<ul>
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 rounded-sm bg-blue-600'>
 						<a
