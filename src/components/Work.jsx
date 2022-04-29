eb70419d67da39dfcc9bd6a694eb1cb9131a0bc8
Arushi Katyal <katy.arushi@gmail.com>
2022-04-29 16:27:07 -0400

    add blurb and tech stack to data, add styling

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,13 +7,15 @@ import tweeter from '../assets/tweeter.png';
 import tinyapp from '../assets/tinyapp.png';
 
 const Work = () => {
-
-  const projects = [
+	const projects = [
 		{
 			url: veggielicious,
 			name: 'Veggielicious',
 			demo: 'https://veggielicious.netlify.app/',
 			code: 'https://github.com/katy-arushi/recipe-app',
+			blurb:
+				'Recipe app featuring vegetarian & vegan recipes. Users can filter by cuisine or use the search bar.',
+			tech: 'React, Styled-Components, Spoonacular API',
 			id: 1,
 		},
 		{
@@ -21,6 +23,9 @@ const Work = () => {
 			name: 'Pomodoro Timer',
 			demo: 'https://pomopets.netlify.app/',
 			code: 'https://github.com/katy-arushi/pomopets-client',
+			blurb:
+				'React productivity app that integrates a Pomodoro Timer and a digital pet.',
+			tech: 'React, SASS, Bootstrap, Node, Express, Prisma',
 			id: 2,
 		},
 		{
@@ -28,6 +33,9 @@ const Work = () => {
 			name: 'Interview Scheduler',
 			demo: 'https://interview-scheduler-2022.netlify.app/',
 			code: 'https://github.com/katy-arushi/scheduler',
+			blurb:
+				'React app for scheduling interviews, features unit, integration and E2E testing.',
+			tech: 'React, Jest, Storybook, Cypress, CircleCI',
 			id: 3,
 		},
 		{
@@ -35,6 +43,9 @@ const Work = () => {
 			name: 'Password Manager',
 			demo: '/',
 			code: 'https://github.com/katy-arushi/passwordb',
+			blurb:
+				"Passwordb is a password manager that allows a user to add accounts and generate secure passwords.",
+			tech: 'JavaScript, HTML, CSS, SASS, jQuery, Node, Express, PostgreSQL',
 			id: 4,
 		},
 		{
@@ -42,6 +53,9 @@ const Work = () => {
 			name: 'Tweeter',
 			demo: '/',
 			code: 'https://github.com/katy-arushi/tweeter',
+			blurb:
+				'Single-page Twitter clone focusing on front-end, responsive design.',
+			tech: 'JavaScript, HTML, CSS, jQuery, AJAX',
 			id: 5,
 		},
 		{
@@ -49,10 +63,12 @@ const Work = () => {
 			name: 'TinyApp URL Shortener',
 			demo: '/',
 			code: 'https://github.com/katy-arushi/tinyapp',
+			blurb: 'App for shortening long URLs, à la bit.ly.',
+			tech: 'JavaScript, EJS, Bootstrap, Node, Express',
 			id: 6,
 		},
 	];
-  
+
 	return (
 		<div name='work' className='w-full md:h-screen text-gray-300 bg-background'>
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -68,30 +84,39 @@ const Work = () => {
 				{/* container */}
 				<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 					{projects.map(project => {
-						return (
-							<div
-								style={{ backgroundImage: `url(${project.url})` }}
-								className='shadow-lg shadow-shadow group container rounded-md flex justify-center items-center mx-auto content-div'
-								key={project.id}
-							>
-								{/* hover effects*/}
-								<div className='opacity-0 group-hover:opacity-100 text-center'>
-									<span className='text-2xl font-bold text-white tracking-wider text-center'>
-										{project.name}
-									</span>
-									<div className='pt-8 text-center'>
-										<a href={project.demo} target='_blank' rel='noreferrer'>
-											<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
-												Demo
-											</button>
-										</a>
-										<a href={project.code} target='_blank' rel='noreferrer'>
-											<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
-												Code
-											</button>
-										</a>
+            return (
+							<div>
+								<div
+									style={{ backgroundImage: `url(${project.url})` }}
+									className='shadow-lg shadow-shadow group container rounded-md flex justify-center items-center mx-auto content-div'
+									key={project.id}
+								>
+									{/* hover effects*/}
+									<div className='opacity-0 group-hover:opacity-100 text-center'>
+										<span className='text-2xl font-bold text-white tracking-wider text-center'>
+											{project.name}
+										</span>
+										<div className='pt-8 text-center'>
+											<a href={project.demo} target='_blank' rel='noreferrer'>
+												<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
+													Demo
+												</button>
+											</a>
+											<a href={project.code} target='_blank' rel='noreferrer'>
+												<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
+													Code
+												</button>
+											</a>
+										</div>
 									</div>
 								</div>
+								<p className='mt-2 p-2 lg:h-[100px] md:h-[100px]'>
+									{project.blurb}
+								</p>
+								<p className='px-2 mt-2'>
+									<span className='font-bold text-pink-600'>Made with: </span>
+									{project.tech}
+								</p>
 							</div>
 						);
 					})}
