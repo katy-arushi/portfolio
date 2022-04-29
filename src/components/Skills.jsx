863c500108ac47399ac91ab1a7cf16632e3ad09e
Arushi Katyal <katy.arushi@gmail.com>
2022-04-29 02:36:41 -0400

    add pink border and padding for skill boxes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,18 +10,24 @@ import Postgres from '../assets/postgres.png';
 
 const Skills = () => {
 	return (
-		<div name='skills' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
+		<div
+			name='skills'
+			className='w-full md:h-screen bg-[#0a192f] text-gray-300'
+		>
 			{/* container */}
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
 				<div>
-					<p className='text-4xl font-bold inline border-b-4 border-pink-600 '>
+					<p className='text-4xl font-bold inline border-b-4 border-pink-600 rounded-md '>
 						Skills
 					</p>
-					<p className='py-4 text-lg'>// These are some of the technologies I've worked with and specialize in</p>
+					<p className='py-4 text-lg'>
+						// These are some of the technologies I've worked with and
+						specialize in
+					</p>
 				</div>
 
 				<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img
 							className='w-20 mx-auto'
 							src={JavaScript}
@@ -29,27 +35,27 @@ const Skills = () => {
 						/>
 						<p className='my-4'>JavaScript</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={ReactImg} alt='React icon' />
 						<p className='my-4'>React</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
 						<p className='my-4'>HTML</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={CSS} alt='CSS icon' />
 						<p className='my-4'>CSS</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={Sass} alt='Sass icon' />
 						<p className='my-9'>Sass</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={Node} alt='Node icon' />
 						<p className='my-4'>Node.js</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img
 							className='w-20 mx-auto'
 							src={Postgres}
@@ -57,7 +63,7 @@ const Skills = () => {
 						/>
 						<p className='my-3.5'>PostgreSQL</p>
 					</div>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+					<div className='shadow-md shadow-skills-shadow border-2 border-pink-600 rounded-md pt-4 hover:scale-110 duration-500'>
 						<img className='w-20 mx-auto' src={GitHub} alt='GitHub icon' />
 						<p className='my-4'>GitHub</p>
 					</div>
