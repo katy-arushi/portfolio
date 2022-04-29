ea1820016efadda9bba2c3f534e7ab75fa2ed72b
Arushi Katyal <katy.arushi@gmail.com>
2022-04-29 02:31:12 -0400

    attempt to map over project array and make dynamic, but couldn't get it working - commented out for now

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,6 +7,52 @@ import tweeter from '../assets/tweeter.png';
 import tinyapp from '../assets/tinyapp.png';
 
 const Work = () => {
+
+  const projects = [
+		{
+			url: 'veggielicious',
+			name: 'Veggielicious',
+			demo: 'https://veggielicious.netlify.app/',
+			code: 'https://github.com/katy-arushi/recipe-app',
+			id: 1,
+		},
+		{
+			url: 'pomopets',
+			name: 'Pomodoro Timer',
+			demo: 'https://pomopets.netlify.app/',
+			code: 'https://github.com/katy-arushi/pomopets-client',
+			id: 2,
+		},
+		{
+			url: 'scheduler',
+			name: 'Interview Scheduler',
+			demo: 'https://interview-scheduler-2022.netlify.app/',
+			code: 'https://github.com/katy-arushi/scheduler',
+			id: 3,
+		},
+		{
+			url: 'passwordb',
+			name: 'Password Manager',
+			demo: '/',
+			code: 'https://github.com/katy-arushi/passwordb',
+			id: 4,
+		},
+		{
+			url: 'tweeter',
+			name: 'Tweeter',
+			demo: '/',
+			code: 'https://github.com/katy-arushi/tweeter',
+			id: 5,
+		},
+		{
+			url: 'tinyapp',
+			name: 'TinyApp URL Shortener',
+			demo: '/',
+			code: 'https://github.com/katy-arushi/tinyapp',
+			id: 6,
+		},
+	];
+  
 	return (
 		<div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -20,9 +66,38 @@ const Work = () => {
 				</div>
 
 				{/* container */}
-				<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-					{/* veggielicious */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          
+      {/* {projects.map(project => {
+        return (
 					<div
+						style={{ backgroundImage: `url(${project.url})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            key={project.id}
+					>
+						{/* hover effects */}
+						{/* <div className='opacity-0 group-hover:opacity-100 text-center'>
+							<span className='text-2xl font-bold text-white tracking-wider text-center'>
+								{project.name}
+							</span>
+							<div className='pt-8 text-center'>
+								<a href={project.demo} target='_blank' rel='noreferrer'>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
+										Demo
+									</button>
+								</a>
+								<a href={project.code} target='_blank' rel='noreferrer'>
+									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-btn-hover'>
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+				);
+      })} */}
+          
+          <div
 						style={{ backgroundImage: `url(${veggielicious})` }}
 						className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
 					>
