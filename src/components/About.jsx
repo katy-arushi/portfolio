ffb26c7f330bcc5b979d351e59e4fb15fd493de8
Arushi Katyal <katy.arushi@gmail.com>
2022-05-04 00:32:56 -0400

    change h-screen to min-h-screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 
 const About = () => {
 	return (
-		<div
-			id='about'
-			className='w-full md:h-screen bg-[#0a192f] text-gray-300'
-		>
-			<div className='flex flex-col justify-center items-center w-full h-full'>
+		<div id='about' className='w-full min-h-screen bg-background text-gray-300'>
+			<div className='flex flex-col justify-center items-center w-full min-h-screen'>
 				<div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
 					<div className='sm:text-right pb-8 pl-4'>
 						<p className='text-4xl font-bold inline border-b-4 border-pink-600'>
