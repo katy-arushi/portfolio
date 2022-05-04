354dc61944a8f746b5be933259559aba44cc1973
Arushi Katyal <katy.arushi@gmail.com>
2022-05-04 02:11:53 -0400

    change padding on y axis to just top

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -76,16 +76,15 @@ const Skills = () => {
 			{/* container */}
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full min-h-screen'>
 				<div>
-					<p className='text-4xl font-bold inline border-b-4 border-pink-600 rounded-md '>
+					<p className='text-4xl font-bold inline border-b-4 border-pink-600 rounded-md'>
 						Skills
 					</p>
 					<p className='py-4 text-lg'>
-						// These are some of the technologies I've worked with and
-						specialize in
+						// These are some of the technologies I've worked with and specialize in
 					</p>
 				</div>
 
-				<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+				<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center pt-8'>
 					{/* skill boxes */}
 
 					{skills.map(skill => {
