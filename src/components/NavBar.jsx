7b3d6c1d42ec3846f66466925cdc0b482ebfe39d
Arushi Katyal <katy.arushi@gmail.com>
2023-02-21 01:54:38 -0500

    capitalize GitHub

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -137,7 +137,7 @@ function NavBar() {
 							target='_blank'
 							rel='noreferrer'
 						>
-							Github <FaGithub size={30} />
+							GitHub <FaGithub size={30} />
 						</a>
 					</li>
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 rounded-sm bg-pink-600'>
